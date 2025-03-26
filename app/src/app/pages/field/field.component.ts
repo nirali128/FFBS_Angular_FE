d8a8d3f7c08df9256a977019136f891994a05e95
Shivani Dave <shivani.dave@tatvasoft.com>
2025-03-26 12:31:56 +0530

    Changes for spacing in field

--- app/src/app/pages/field/field.component.ts
+++ app/src/app/pages/field/field.component.ts
@@ -1,18 +1,25 @@
 import { Component, ViewChild } from '@angular/core';
 import { FIELD_COLUMNS, FieldsDetailList } from '../../shared/interfaces/field';
 import { FieldService } from '../../shared/service/field.service';
-import { ApiPaginatedResponse, PaginationRequest } from '../../shared/interfaces/api.response';
+import {
+  ApiPaginatedResponse,
+  PaginationRequest,
+} from '../../shared/interfaces/api.response';
 import { DataViewComponent } from '../../shared/components/data-view/data-view.component';
 import { Router } from '@angular/router';
 import { ButtonComponent } from '../../shared/components/button/button.component';
-import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import {
+  MatPaginator,
+  MatPaginatorModule,
+  PageEvent,
+} from '@angular/material/paginator';
 import { AuthService } from '../../shared/service/authentication.service';
 
 @Component({
   selector: 'app-field',
   imports: [DataViewComponent, ButtonComponent, MatPaginatorModule],
   templateUrl: './field.component.html',
-  styleUrl: './field.component.scss'
+  styleUrl: './field.component.scss',
 })
 export class FieldComponent {
   fields: FieldsDetailList[];
@@ -21,7 +28,7 @@ export class FieldComponent {
     address: 'location_on',
     area: 'straighten',
     phone: 'phone',
-    rules: 'gavel'
+    rules: 'gavel',
   };
   page = 1;
   pageSize = 3;
@@ -33,8 +40,11 @@ export class FieldComponent {
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
-  constructor(public readonly fieldService: FieldService, public router: Router, public readonly authService: AuthService) {
-  }
+  constructor(
+    public readonly fieldService: FieldService,
+    public router: Router,
+    public readonly authService: AuthService
+  ) {}
 
   ngOnInit(): void {
     this.loadFields();
@@ -49,25 +59,27 @@ export class FieldComponent {
       sortOrder: this.sortOrder,
     };
 
-    this.fieldService.getAllFields(params).subscribe((res: ApiPaginatedResponse<FieldsDetailList[]>) => {
-      if (res.success) {
-        this.fields = res.data;
-        this.totalItems = res.pagination.totalItems;
-        this.totalPages = res.pagination.totalPages;
-      }
-    });
+    this.fieldService
+      .getAllFields(params)
+      .subscribe((res: ApiPaginatedResponse<FieldsDetailList[]>) => {
+        if (res.success) {
+          this.fields = res.data;
+          this.totalItems = res.pagination.totalItems;
+          this.totalPages = res.pagination.totalPages;
+        }
+      });
   }
 
   navigate(guid: string) {
-    this.router.navigateByUrl("/field/booking/" + guid)
+    this.router.navigateByUrl('/field/booking/' + guid);
   }
 
   navigateToAddField() {
-    this.router.navigateByUrl("/field/add");
+    this.router.navigateByUrl('/field/add');
   }
 
   edit(guid: string) {
-    this.router.navigateByUrl("/field/edit/" + guid)
+    this.router.navigateByUrl('/field/edit/' + guid);
   }
 
   onPageChange(event: PageEvent): void {
@@ -76,10 +88,9 @@ export class FieldComponent {
     this.loadFields();
   }
 
-  
-onSearch(query: string) {
-  this.searchQuery = query;
-  this.page = 1;
-  this.loadFields();
-}
+  onSearch(query: string) {
+    this.searchQuery = query;
+    this.page = 1;
+    this.loadFields();
+  }
 }
